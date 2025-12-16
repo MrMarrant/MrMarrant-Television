d677a4532b2a36b821dbbdb896d5a419ecad813e
MrMarrant <laughymclaugher@gmail.com>
2025-12-16 21:33:49 +0100

    [UPDATE] Node Version

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'cdn.jsdelivr.net', 'i.imgur.com', 'www.transparenttextures.com'],
-  },
-};
-
-export default nextConfig;
